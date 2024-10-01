ab014944b94021590275c4fe71d01f36c2592e89
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2024-10-01 20:22:26 -0300

    TP final módulo 1 - ejercicio 2 listo

--- tpFinalModulo1.php
+++ tpFinalModulo1.php
@@ -50,14 +50,16 @@ function verificarNumero($numero){
             }
         }
         //respuesta condicional segun si es primo o no
+        /*
         if($esPrimo){
             echo "El número es $esPar y primo";
         } else {
             echo "El número es $esPar pero NO primo";
         }
+        */
         return [$esPrimo, $esPar];
     } else {
-        echo "El número verificado NO es entero y TAMPOCO mayor a 1";
+        //echo "El número verificado NO es entero y TAMPOCO mayor a 1 <br>";
         return [$esPrimo, $esPar];
     }
 
@@ -87,21 +89,46 @@ el rango.
 ▪ Un arreglo con los números primos pares.
 ▪ Un arreglo con los números primos impares.
 4. Salida:
-o Muestra por pantalla:
+▪ Muestra por pantalla:
 ▪ La cantidad total de números primos encontrados.
 ▪ La cantidad de números primos pares.
 ▪ La cantidad de números primos impares.
 */
 //---------------------------------------------------------------------------------------------------------------------------------------------
-
-
+function verificarPrimos($inicio, $fin){
+    //iniciamos arrays vacios para guardar los resultados de la iteración
+    $primos = [];
+    $primosPares = [];
+    $primosImpares = [];
+    //verificamos usando la función verificarNumero() si cada número en la iteración es primo, primo par o primo impar y guardamos en el
+    //array correspondiente 
+    for ($i=$inicio; $i <= $fin; $i++) { 
+        $resultado = verificarNumero($i);
+        if($resultado[0] == true && $resultado[1] == "par"){
+            array_push($primos, $i);
+            array_push($primosPares, $i);
+        } elseif ($resultado[0] == true && $resultado[1] == "impar"){
+            array_push($primos, $i);
+            array_push($primosImpares, $i);
+        }        
+    }
+    //imprimimos los resultados
+    echo "Números primos encontrados entre $inicio y $fin = " .implode(", ", $primos);
+    echo "<br>Cantidad de números primos = " .count($primos);
+    echo "<br>Cantidad de números primos pares = " .count($primosPares);
+    echo "<br>Cantidad de números primos impares = " .count($primosImpares);
+}
 //---------------------------------------------------------------------------------------------------------------------------------------------
 
 //------------------------------------------------------EJECUCION DE FUNCIONES-----------------------------------------------------------------
-verificarNumero(15)
-
+verificarNumero(15);
+verificarPrimos(2, 15);
 
 //--------------------------------------------------------------CONSULTAS----------------------------------------------------------------------
-//https://www.php.net/manual/es/function.is-int.php
+//Se usaron las siguientes funciones de la documentación oficial de php: 
+//https://www.php.net/manual/es/function.is-int.php (para saber si el número es un entero)
+//https://www.php.net/manual/es/function.array-push.php (para guardar elementos en un array)
+//https://www.php.net/manual/es/function.count.php (para contar los elementos de un array)
+//https://www.php.net/manual/es/function.implode.php (para juntar e imprimir los elementos de un array)
 
 ?>
